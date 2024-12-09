2ba1eb17eff6b93d154abc018bec7d796d4c50d4
Willy Wilsen <willywilsen.ww@gmail.com>
2024-12-09 20:40:12 +0700

    feat: LinkedIn Improvement on the view

--- addons/bmw_crm/models/crm_lead.py
+++ addons/bmw_crm/models/crm_lead.py
@@ -9,6 +9,7 @@ _logger = logging.getLogger(__name__)
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
+    linkedin_results_visibility = fields.Boolean(string="LinkedIn Results Visibility", default=False)
     linkedin_results = fields.One2many('crm.linkedin.result', 'lead_id', string="LinkedIn Results")
     linkedin_preview_title = fields.Char(string="Title", readonly=True)
     linkedin_preview_link = fields.Char(string="Link", readonly=True)
@@ -70,47 +71,51 @@ class CrmLead(models.Model):
                 }
                 scraping_linkedin_api_url = os.getenv('SCRAPING_API_URL') + '/linkedin'
                 scraping_linkedin_api_response = requests.get(scraping_linkedin_api_url, params=scraping_linkedin_api_param).json()
-                scraping_linkedin_result = {}
+                scraping_linkedin_result = None
                 if isinstance(scraping_linkedin_api_response, list) and len(scraping_linkedin_api_response) > 0:
                     scraping_linkedin_result = scraping_linkedin_api_response[0]
 
-                linkedin_result = record.linkedin_results.create({
-                    'lead_id': record.id,
-                    'title': scraping_google_result.get('title', ''),
-                    'link': scraping_google_result.get('link', ''),
-                    'full_name': scraping_linkedin_result.get('fullName', ''),
-                    'profile_photo_preview': utils.file.convert_url_to_base64(scraping_linkedin_result.get('profile_photo', '')),
-                    'profile_photo': scraping_linkedin_result.get('profile_photo', ''),
-                    'headline': scraping_linkedin_result.get('headline', ''),
-                    'about': scraping_linkedin_result.get('about', ''),
-                    'location': scraping_linkedin_result.get('location', ''),
-                    'description1': scraping_linkedin_result.get('description', {}).get('description1', ''),
-                    'description1_link': scraping_linkedin_result.get('description', {}).get('description1_link', ''),
-                    'description2': scraping_linkedin_result.get('description', {}).get('description2', ''),
-                    'description2_link': scraping_linkedin_result.get('description', {}).get('description2_link', ''),
-                })
-                for experience in scraping_linkedin_result.get('experience', []):
-                    linkedin_result.experiences.create({
-                        'linkedin_result_id': linkedin_result.id,
-                        'position': experience.get('position', ''),
-                        'company_name': experience.get('company_name', ''),
-                        'company_url': experience.get('company_url', ''),
-                        'location': experience.get('location', ''),
-                        'starts_at': experience.get('starts_at', ''),
-                        'duration': experience.get('duration', ''),
+                if scraping_linkedin_result:
+                    linkedin_result = record.linkedin_results.create({
+                        'lead_id': record.id,
+                        'title': scraping_google_result.get('title', ''),
+                        'link': scraping_google_result.get('link', ''),
+                        'full_name': scraping_linkedin_result.get('fullName', ''),
+                        'profile_photo_preview': utils.file.convert_url_to_base64(scraping_linkedin_result.get('profile_photo', '')),
+                        'profile_photo': scraping_linkedin_result.get('profile_photo', ''),
+                        'headline': scraping_linkedin_result.get('headline', ''),
+                        'about': scraping_linkedin_result.get('about', ''),
+                        'location': scraping_linkedin_result.get('location', ''),
+                        'description1': scraping_linkedin_result.get('description', {}).get('description1', ''),
+                        'description1_link': scraping_linkedin_result.get('description', {}).get('description1_link', ''),
+                        'description2': scraping_linkedin_result.get('description', {}).get('description2', ''),
+                        'description2_link': scraping_linkedin_result.get('description', {}).get('description2_link', ''),
                     })
-                for education in scraping_linkedin_result.get('education', []):
-                    linkedin_result.educations.create({
-                        'linkedin_result_id': linkedin_result.id,
-                        'college_name': education.get('college_name', ''),
-                        'college_url': education.get('college_url', ''),
-                        'college_degree': education.get('college_degree', ''),
-                        'college_degree_field': education.get('college_degree_field', ''),
-                        'college_duration': education.get('college_duration', ''),
-                    })
+                    for experience in scraping_linkedin_result.get('experience', []):
+                        linkedin_result.experiences.create({
+                            'linkedin_result_id': linkedin_result.id,
+                            'position': experience.get('position', ''),
+                            'company_name': experience.get('company_name', ''),
+                            'company_url': experience.get('company_url', ''),
+                            'location': experience.get('location', ''),
+                            'starts_at': experience.get('starts_at', ''),
+                            'duration': experience.get('duration', ''),
+                        })
+                    for education in scraping_linkedin_result.get('education', []):
+                        linkedin_result.educations.create({
+                            'linkedin_result_id': linkedin_result.id,
+                            'college_name': education.get('college_name', ''),
+                            'college_url': education.get('college_url', ''),
+                            'college_degree': education.get('college_degree', ''),
+                            'college_degree_field': education.get('college_degree_field', ''),
+                            'college_duration': education.get('college_duration', ''),
+                        })
+
+            record.linkedin_results_visibility = True
 
     def set_as_linkedin_url(self):
         _logger.info("Setting as LinkedIn URL")
         for record in self:
             if record.linkedin_preview_link:
                 record.linkedin_url = record.linkedin_preview_link
+                record.linkedin_results_visibility = False
