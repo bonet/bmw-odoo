0ee1fd210479385c4fb9663ef64dbcc449204897
Willy Wilsen <willywilsen.ww@gmail.com>
2024-12-12 20:42:36 +0700

    feat: LinkedIn Improvement

--- addons/bmw_crm/controllers/crm_lead.py
+++ addons/bmw_crm/controllers/crm_lead.py
@@ -39,10 +39,11 @@ class CrmLeadController(http.Controller):
                 'api_key': os.getenv('SCRAPING_API_KEY'),
                 'type': 'profile',
                 'linkId': link_id,
-                'private': 'false'
+                'private': 'true'
             }
             scraping_linkedin_api_url = os.getenv('SCRAPING_API_URL') + '/linkedin'
             scraping_linkedin_api_response = requests.get(scraping_linkedin_api_url, params=scraping_linkedin_api_param).json()
+            _logger.info(scraping_linkedin_api_response)
             scraping_linkedin_result = None
             if isinstance(scraping_linkedin_api_response, list) and len(scraping_linkedin_api_response) > 0:
                 scraping_linkedin_result = scraping_linkedin_api_response[0]
@@ -61,42 +62,43 @@ class CrmLeadController(http.Controller):
             
             data = payload.get('data', {})
             crm_lead_record.write({
-                'linkedin_preview_title': payload.get('title', ''),
-                'linkedin_preview_link': payload.get('link', ''),
-                'linkedin_preview_full_name': data.get('fullName', ''),
-                'linkedin_preview_profile_photo_preview': utils.file.convert_url_to_base64(data.get('profile_photo', '')),
-                'linkedin_preview_profile_photo': data.get('profile_photo', ''),
-                'linkedin_preview_headline': data.get('headline', ''),
-                'linkedin_preview_about': data.get('about', ''),
-                'linkedin_preview_location': data.get('location', ''),
-                'linkedin_preview_description1': data.get('description', {}).get('description1', ''),
-                'linkedin_preview_description1_link': data.get('description', {}).get('description1_link', ''),
-                'linkedin_preview_description2': data.get('description', {}).get('description2', ''),
-                'linkedin_preview_description2_link': data.get('description', {}).get('description2_link', ''),
+                'linkedin_title': payload.get('title', '') if payload.get('title', '') else 'N/A',
+                'linkedin_link': payload.get('link', '') if payload.get('link', '') else 'N/A',
+                'linkedin_full_name': data.get('fullName', '') if data.get('fullName', '') else 'N/A',
+                'linkedin_profile_photo_preview': utils.file.convert_url_to_base64(data.get('profile_photo', '')),
+                'linkedin_profile_photo': data.get('profile_photo', '') if data.get('profile_photo', '') else 'N/A',
+                'linkedin_headline': data.get('headline', '') if data.get('headline', '') else 'N/A',
+                'linkedin_about': data.get('about', '') if data.get('about', '') else 'N/A',
+                'linkedin_location': data.get('location', '') if data.get('location', '') else 'N/A',
+                'linkedin_description1': data.get('description', {}).get('description1', '') if data.get('description', {}).get('description1', '') else 'N/A',
+                'linkedin_description1_link': data.get('description', {}).get('description1_link', '') if data.get('description', {}).get('description1_link', '') else 'N/A',
+                'linkedin_description2': data.get('description', {}).get('description2', '') if data.get('description', {}).get('description2', '') else 'N/A',
+                'linkedin_description2_link': data.get('description', {}).get('description2_link', '') if data.get('description', {}).get('description2_link', '') else 'N/A',
+                'linkedin_url': payload.get('link', '') if payload.get('link', '') else 'N/A',
             })
 
-            crm_lead_record.linkedin_preview_experiences.unlink()
-            crm_lead_record.linkedin_preview_educations.unlink()
+            crm_lead_record.linkedin_experiences.unlink()
+            crm_lead_record.linkedin_educations.unlink()
 
             for experience in data.get('experience', []):
-                crm_lead_record.linkedin_preview_experiences.create({
+                crm_lead_record.linkedin_experiences.create({
                     'lead_id': crm_lead_record.id,
-                    'position': experience.get('position', ''),
-                    'company_name': experience.get('company_name', ''),
-                    'company_url': experience.get('company_url', ''),
-                    'location': experience.get('location', ''),
-                    'starts_at': experience.get('starts_at', ''),
-                    'duration': experience.get('duration', ''),
+                    'position': experience.get('position', '') if experience.get('position', '') else 'N/A',
+                    'company_name': experience.get('company_name', '') if experience.get('company_name', '') else 'N/A',
+                    'company_url': experience.get('company_url', '') if experience.get('company_url', '') else 'N/A',
+                    'location': experience.get('location', '') if experience.get('location', '') else 'N/A',
+                    'starts_at': experience.get('starts_at', '') if experience.get('starts_at', '') else 'N/A',
+                    'duration': experience.get('duration', '') if experience.get('duration', '') else 'N/A',
                 })
 
             for education in data.get('education', []):
-                crm_lead_record.linkedin_preview_educations.create({
+                crm_lead_record.linkedin_educations.create({
                     'lead_id': crm_lead_record.id,
-                    'college_name': education.get('college_name', ''),
-                    'college_url': education.get('college_url', ''),
-                    'college_degree': education.get('college_degree', ''),
-                    'college_degree_field': education.get('college_degree_field', ''),
-                    'college_duration': education.get('college_duration', ''),
+                    'college_name': education.get('college_name', '') if education.get('college_name', '') else 'N/A',
+                    'college_url': education.get('college_url', '') if education.get('college_url', '') else 'N/A',
+                    'college_degree': education.get('college_degree', '') if education.get('college_degree', '') else 'N/A',
+                    'college_degree_field': education.get('college_degree_field', '') if education.get('college_degree_field', '') else 'N/A',
+                    'college_duration': education.get('college_duration', '') if education.get('college_duration', '') else 'N/A',
                 })
 
             return {"success": "Data updated successfully"}
--- addons/bmw_crm/models/crm_lead.py
+++ addons/bmw_crm/models/crm_lead.py
@@ -6,31 +6,33 @@ _logger = logging.getLogger(__name__)
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    linkedin_preview_title = fields.Char(string="Title", readonly=True)
-    linkedin_preview_link = fields.Char(string="Link", readonly=True)
-    linkedin_preview_full_name = fields.Char(string="Full Name", readonly=True)
-    linkedin_preview_profile_photo_preview = fields.Image(string="Profile Photo Preview", readonly=True)
-    linkedin_preview_profile_photo = fields.Char(string="Profile Photo", readonly=True)
-    linkedin_preview_headline = fields.Char(string="Headline", readonly=True)
-    linkedin_preview_about = fields.Text(string="About", readonly=True)
-    linkedin_preview_location = fields.Char(string="Location", readonly=True)
-    linkedin_preview_description1 = fields.Text(string="Description 1", readonly=True)
-    linkedin_preview_description1_link = fields.Char(string="Description 1 Link", readonly=True)
-    linkedin_preview_description2 = fields.Text(string="Description 2", readonly=True)
-    linkedin_preview_description2_link = fields.Char(string="Description 2 Link", readonly=True)
-    linkedin_preview_experiences = fields.One2many('crm.linkedin.experience.preview', 'lead_id', string="Experiences")
-    linkedin_preview_educations = fields.One2many('crm.linkedin.education.preview', 'lead_id', string="Educations")
+    linkedin_title = fields.Char(string="Title", readonly=True)
+    linkedin_link = fields.Char(string="Link", readonly=True)
+    linkedin_full_name = fields.Char(string="Full Name", readonly=True)
+    linkedin_profile_photo_preview = fields.Image(string="Profile Photo Preview", readonly=True)
+    linkedin_profile_photo = fields.Char(string="Profile Photo", readonly=True)
+    linkedin_headline = fields.Char(string="Headline", readonly=True)
+    linkedin_about = fields.Text(string="About", readonly=True)
+    linkedin_location = fields.Char(string="Location", readonly=True)
+    linkedin_description1 = fields.Text(string="Description 1", readonly=True)
+    linkedin_description1_link = fields.Char(string="Description 1 Link", readonly=True)
+    linkedin_description2 = fields.Text(string="Description 2", readonly=True)
+    linkedin_description2_link = fields.Char(string="Description 2 Link", readonly=True)
+    linkedin_experiences_count = fields.Integer(string="Experiences Count", compute='_compute_linkedin_experiences_count')
+    linkedin_experiences = fields.One2many('crm.linkedin.experience.preview', 'lead_id', string="Experiences")
+    linkedin_educations_count = fields.Integer(string="Educations Count", compute='_compute_linkedin_educations_count')
+    linkedin_educations = fields.One2many('crm.linkedin.education.preview', 'lead_id', string="Educations")
     linkedin_url = fields.Char(string="LinkedIn URL")
 
-    @api.onchange('linkedin_preview_link')
-    def onchange(self):
-        _logger.info("Onchange")
-
-    def load_linkedin(self):
+    def research_user_linkedin(self):
         pass
 
-    def set_as_linkedin_url(self):
-        _logger.info("Setting as LinkedIn URL")
+    @api.depends('linkedin_experiences')
+    def _compute_linkedin_experiences_count(self):
         for record in self:
-            if record.linkedin_preview_link:
-                record.linkedin_url = record.linkedin_preview_link
+            record.linkedin_experiences_count = len(record.linkedin_experiences)
+
+    @api.depends('linkedin_educations')
+    def _compute_linkedin_educations_count(self):
+        for record in self:
+            record.linkedin_educations_count = len(record.linkedin_educations)
